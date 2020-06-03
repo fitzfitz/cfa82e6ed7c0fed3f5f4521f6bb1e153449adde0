ec48a7b6b85abf6dad972628c76a2dbf873efc42
Fitzgeral <fitzgeralmedia@gmail.com>
2020-06-03 22:41:32 +0700

    Updates

---
+++ precache-manifest.fd23bdeaad4896a544a6492c8c86203b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187eb5e352587345a788bab27b8c81f2",
+    "url": "/cfa82e6ed7c0fed3f5f4521f6bb1e153449adde0/index.html"
+  },
+  {
+    "revision": "757f74dea5bd06f031e4",
+    "url": "/cfa82e6ed7c0fed3f5f4521f6bb1e153449adde0/static/css/main.c1a3bb2f.chunk.css"
+  },
+  {
+    "revision": "367d7f04e8e043aab5b5",
+    "url": "/cfa82e6ed7c0fed3f5f4521f6bb1e153449adde0/static/js/2.43dae6cd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cfa82e6ed7c0fed3f5f4521f6bb1e153449adde0/static/js/2.43dae6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757f74dea5bd06f031e4",
+    "url": "/cfa82e6ed7c0fed3f5f4521f6bb1e153449adde0/static/js/main.bf9cc0a4.chunk.js"
+  },
+  {
+    "revision": "a2f0eb89d570d93913ee",
+    "url": "/cfa82e6ed7c0fed3f5f4521f6bb1e153449adde0/static/js/runtime-main.b2b6103a.js"
+  }
+]);
